b807c84956659adb1122cefb26f789eef57ab7f1
Gyokujin <dd43454548@naver.com>
2022-10-25 05:18:01 +0900

    #몬스터 업데이트

---
+++ Assets/Scripts/Enemy/BurningGhoul.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurningGhoul : Enemy
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

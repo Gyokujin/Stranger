08022ca1bf129c340429c1ad8a61b25587c3fa7c
Gyokujin <dd43454548@naver.com>
2022-10-30 11:33:25 +0900

    #포탈 업데이트

---
+++ Assets/Scripts/Object/Object.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    public enum ObjectType { TreasureBox, PortalRing }
+}
--- Assets/Scripts/Object/Portal.cs
+++ Assets/Scripts/Object/Portal.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Portal : MonoBehaviour
+public class Portal : Object
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
